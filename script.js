052e51b21529c1ce944b652182393cb40385c040
James Deal <alphaencryption89@protonmail.com>
2018-06-19 16:36:28 +0800

    added functionality

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
+var check = false;
+
 btn1.onclick = function() {
-	input.value = input.value + 1;
+	updateScreen(1);
 };
 
 btn2.onclick = function() {
-	input.value = input.value + 2;
+	updateScreen(2);
 };
 
 btn3.onclick = function() {
-	input.value = input.value + 3;
+	updateScreen(3);
 };
 
 btn4.onclick = function() {
-	input.value = input.value + 4;
+	updateScreen(4);
 };
 
 btn5.onclick = function() {
-	input.value = input.value + 5;
+	updateScreen(5);
 };
 
 btn6.onclick = function() {
-	input.value = input.value + 6;
+	updateScreen(6);
 };
 
 btn7.onclick = function() {
-	input.value = input.value + 7;
+	updateScreen(7);
 };
 
 btn8.onclick = function() {
-	input.value = input.value + 8;
+	updateScreen(8);
 };
 
 btn9.onclick = function() {
-	input.value = input.value + 9;
+	updateScreen(9);
 };
 
 btn0.onclick = function() {
-	input.value = input.value + 0;
+	updateScreen(0);
 };
 
 min.onclick = function() {
@@ -52,4 +54,19 @@ pls.onclick = function() {
 
 cls.onclick = function() {
 	input.value = null;
+};
+
+equal.onclick = function() {
+	var result = eval(input.value);
+	input.value = result;
+	check = true;
+};
+
+function updateScreen(key) {
+	if (check == true) {
+		input.value = key;
+		check = false;
+	} else {
+		input.value += key;
+	}
 };
