0d4f42f21d64e6932bf74ffe908d9ce03d1a346b
James Deal <alphaencryption89@protonmail.com>
2018-06-19 16:03:29 +0800

    added buttons

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-alert("hello");
+btn1.onclick = function() {
+	input.value = input.value + 1;
+};
+
+btn2.onclick = function() {
+	input.value = input.value + 2;
+};
+
+btn3.onclick = function() {
+	input.value = input.value + 3;
+};
+
+btn4.onclick = function() {
+	input.value = input.value + 4;
+};
+
+btn5.onclick = function() {
+	input.value = input.value + 5;
+};
+
+btn6.onclick = function() {
+	input.value = input.value + 6;
+};
+
+btn7.onclick = function() {
+	input.value = input.value + 7;
+};
+
+btn8.onclick = function() {
+	input.value = input.value + 8;
+};
+
+btn9.onclick = function() {
+	input.value = input.value + 9;
+};
+
+btn0.onclick = function() {
+	input.value = input.value + 0;
+};
+
+min.onclick = function() {
+	input.value = input.value + "-";
+};
+
+divi.onclick = function() {
+	input.value = input.value + "/";
+};
+
+pls.onclick = function() {
+	input.value = input.value + "+";
+};
+
+cls.onclick = function() {
+	input.value = null;
+};
